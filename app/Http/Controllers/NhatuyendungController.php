f45d6df9e9641e7f86c2c6cab08ae80a394dabf3
tuchithanh1998 <tuchithanh1998@gmail.com>
2020-07-24 11:24:37 +0700

    ungvien change anh

--- app/Http/Controllers/NhatuyendungController.php
+++ app/Http/Controllers/NhatuyendungController.php
@@ -29,6 +29,26 @@ class NhatuyendungController extends Controller
 	
 	public function postDoimatkhau(Request $request)
 	{
+
+			 if (!(Hash::check($request->oldpassword,Auth::guard('nhatuyendung')->user()->matkhau))) 
+    return redirect()->back()->with('alert','Mật khẩu hiện tại không khớp.');
+
+     $this->validate($request,[
+  'oldpassword'=>'required',
+  'newpassword1'=>'required|min:8',
+  'newpassword2'=>'required|same:newpassword1|different:oldpassword|min:8',
+],[
+  'oldpassword.required'=>'Mật khẩu không được để trống.',
+  'newpassword1.required'=>'Mật khẩu không được để trống.',
+  'newpassword2.required'=>'Mật khẩu không được để trống.',
+  'oldpassword.min'=>'Mật khẩu tối tiểu 8 kí tự.',
+  'newpassword1.min'=>'Mật khẩu tối tiểu 8 kí tự.',
+  'newpassword2.min'=>'Mật khẩu tối tiểu 8 kí tự.',
+  'newpassword2.same'=>'Mật khẩu mới không khớp.',
+  'newpassword2.different'=>'Mật khẩu mới phải khác mật khẩu cũ.',
+]);
+
+
 		nhatuyendung::where('id',Auth::guard('nhatuyendung')->user()->id)->update(['matkhau'=>bcrypt($request->newpassword2)]);
 		return redirect()->back();
 
