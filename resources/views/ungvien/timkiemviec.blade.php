058cf67b0f766b07da3da07ab5fb44647414062f
tuchithanh1998 <tuchithanh1998@gmail.com>
2020-08-18 12:41:56 +0700

    timssdsưdd

--- resources/views/ungvien/timkiemviec.blade.php
+++ resources/views/ungvien/timkiemviec.blade.php
@@ -105,7 +105,7 @@
 
 	<div class="row bg-light" style="padding-top:50px; padding-bottom: 50px; margin-top: 25px; margin-bottom: 25px;">
 		<div class="col-sm-10 offset-sm-1 ">
-			<ul class="list-group list-group-flush w-100"  <?php  if(isset($data)==true&&count($data)>12) { ?> id="demo" <?php } ?> >
+			<ul class="list-group list-group-flush w-100"  <?php  if(isset($data)==true&&count($data)>=11) { ?> id="demo" <?php } ?> >
 
 				<?php 
 				if(isset($data)==true)
