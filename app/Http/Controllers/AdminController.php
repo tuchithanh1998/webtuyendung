fde1322c4f556277240c2075a9482fb7cfb4c6a2
tuchithanh1998 <tuchithanh1998@gmail.com>
2020-08-27 23:15:52 +0700

    ungvien admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -597,6 +597,7 @@ $tintuyendung=tintuyendung::findOrFail($id_tintuyendung);
       'khoa'=> $khoa,    
       'ten'=> $ungvien->hoten,    
       'taikhoan'=>$ungvien->email,
+      'nd'=' ',
     ];
    
     \Mail::to($ungvien->email)->send(new \App\Mail\Mailkhoataikhoan($data));
